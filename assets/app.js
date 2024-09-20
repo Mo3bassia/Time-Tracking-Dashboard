961e013ca2267233d7d73cbcbcb3e476f21f9f0c
Mohamed Abassia <mo3bassia@gmail.com>
2024-09-20 18:45:06 +0300

    Finish Js

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,39 @@
+let allCards = document.querySelectorAll(".main-card");
+let choicesList = document.querySelector(".choises ul");
+let all = [];
+
 async function getData() {
   let data = await fetch("data.json");
   let json = await data.json();
-  console.log(json);
+  json.forEach((item, index) => {
+    all.push(item);
+  });
+  showData();
+}
+
+function showData() {
+  let active = choicesList.querySelector("li.active").textContent.toLowerCase();
+  all.forEach((item, index) => {
+    console.log(item);
+    allCards[index].querySelector(".card_title span").textContent = item.title;
+    allCards[index].querySelector(".card_desc .card_hours span").textContent =
+      item.timeframes[active].current;
+    allCards[index].querySelector(
+      ".card_desc .card_previous span"
+    ).textContent = item.timeframes[active].previous;
+    console.log(item.timeframes[active].current);
+  });
 }
 
 getData();
+
+choicesList.querySelectorAll("li").forEach((li) => {
+  li.onclick = function () {
+    choicesList.querySelectorAll("li").forEach((li) => {
+      li.classList.remove("active");
+      li.classList.remove("inactive");
+    });
+    this.classList.add("active");
+    showData();
+  };
+});
